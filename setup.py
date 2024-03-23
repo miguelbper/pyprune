68038a82ae9b14e8e7c9571238588c8525a1fbce
Miguel Pereira <miguel.b.per@gmail.com>
2024-03-23 15:06:57 +0000

    Add project long description

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 from setuptools import setup, find_packages
+from os import path
+
+# read the contents of your README file
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 
 setup(
     name='pyprune',
@@ -7,6 +14,11 @@ setup(
     author='Miguel Pereira',
     author_email='miguel.b.per@gmail.com',
     description='Backtracking algorithm for constraint satisfaction puzzles.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=find_packages(),
     install_requires=['numpy', 'numba'],
+    project_urls={
+        'Source': 'https://github.com/miguelbper/pyprune',
+    },
 )

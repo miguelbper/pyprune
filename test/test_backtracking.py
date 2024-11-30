d7e778e1d7c24a18b697aa7d60f89b83c99f7f3a
Miguel Pereira <miguel.b.per@gmail.com>
2024-11-30 20:17:42 +0000

    Refactor unit tests

--- test/test_backtracking.py
+++ test/test_backtracking.py
@@ -9,12 +9,12 @@ from pyprune.subset import is_singleton, smallest
 
 class Simple:
     @staticmethod
-    def grid_(cm: Choices) -> Grid:
+    def grid(cm: Choices) -> Grid:
         arr = np.array([[smallest(s) for s in row] for row in cm])
         return arr.astype(np.uint32)
 
     @staticmethod
-    def accept_(cm: Choices) -> np.bool:
+    def accept(cm: Choices) -> np.bool:
         bool_arr = np.array([[is_singleton(s) for s in row] for row in cm])
         return np.all(bool_arr)
 
@@ -35,24 +35,46 @@ def cm(request, n: int, k: int) -> Choices:
     return rng.integers(0, 2**k, (n, n), dtype=np.uint32)
 
 
-class TestBacktracking:
-    def test_accept(self, cm: Choices):
-        if np.all(cm):
-            assert Backtracking.accept(cm) == Simple.accept_(cm)
+@pytest.fixture(params=list(range(5)), ids=lambda x: f"[seed={x}]")
+def cm_singletons(request, n: int, k: int) -> Choices:
+    rng = np.random.default_rng(request.param)
+    return 2 ** rng.integers(0, k, (n, n), dtype=np.uint32)
 
-    def test_expand(self, cm: Choices):
-        if np.all(cm) and not np.all(np.logical_and(cm, cm & (cm - 1) == 0)):
-            cms = np.stack(Backtracking.expand(cm), axis=0)
-            comparisons = cms == cm
-            comparison = comparisons[0]
-            assert np.all(comparisons == comparison)
-            assert np.sum(~comparison) == 1
-            multi_index = np.where(~comparison)
-            assert np.sum(cms[:, *multi_index]) == cm[multi_index]
+
+@pytest.fixture(params=list(range(5)), ids=lambda x: f"[seed={x}]")
+def cm_nonzeros(request, n: int, k: int) -> Choices:
+    rng = np.random.default_rng(request.param)
+    return rng.integers(1, 2**k, (n, n), dtype=np.uint32)
+
+
+class TestBacktracking:
+    def test_grid(self, cm_singletons: Choices):
+        assert np.array_equal(Backtracking.grid(cm_singletons), Simple.grid(cm_singletons))
+
+    def test_accept(self, cm_nonzeros: Choices):
+        assert Backtracking.accept(cm_nonzeros) == Simple.accept(cm_nonzeros)
+
+    def test_reject(self):
+        assert Backtracking.reject(None)
+        assert Backtracking.reject(np.zeros((2, 2)))
+        assert Backtracking.reject(np.eye(2))
+        assert not Backtracking.reject(np.ones((2, 2)))
+
+    def test_expand(self, cm_nonzeros: Choices):
+        if np.all(cm_nonzeros & (cm_nonzeros - 1) == 0):
+            return
+        cms = np.stack(Backtracking.expand(cm_nonzeros), axis=0)
+        comparisons = cms == cm_nonzeros
+        comparison = comparisons[0]
+        assert np.all(comparisons == comparison)
+        assert np.sum(~comparison) == 1
+        multi_index = np.where(~comparison)
+        assert np.sum(cms[:, *multi_index]) == cm_nonzeros[multi_index]
 
 
 numba_functions = [
     Backtracking.grid,
+    Backtracking.reject,
     Backtracking.accept,
 ]
 

7a90599dd3ac0262cc05f9edd2727bdf0bcd0a62
Miguel Pereira <miguel.b.per@gmail.com>
2025-01-04 19:25:53 +0000

    Add generic integration tests for backtracking

--- test/test_integration.py
+++ test/test_integration.py
@@ -1,9 +1,11 @@
-from pyprune.backtracking import ArrayBitMask, Backtracking
+import numpy as np
+
+from pyprune.backtracking import ArrayBitMask, Backtracking, Int
 
 
 class NothingIsSolution(Backtracking):
-    def __init__(self, cm: ArrayBitMask) -> None:
-        self.stack = [cm]
+    def __init__(self) -> None:
+        super().__init__()
 
     @staticmethod
     def prune(cm: ArrayBitMask) -> ArrayBitMask | None:
@@ -11,8 +13,8 @@ class NothingIsSolution(Backtracking):
 
 
 class EverythingIsSolution(Backtracking):
-    def __init__(self, cm: ArrayBitMask) -> None:
-        self.stack = [cm]
+    def __init__(self) -> None:
+        super().__init__()
 
     @staticmethod
     def prune(cm: ArrayBitMask) -> ArrayBitMask | None:
@@ -20,9 +22,79 @@ class EverythingIsSolution(Backtracking):
 
 
 class OnlyZeros(Backtracking):
-    def __init__(self, cm: ArrayBitMask) -> None:
-        self.stack = [cm]
+    def __init__(self) -> None:
+        super().__init__()
 
     @staticmethod
     def prune(cm: ArrayBitMask) -> ArrayBitMask | None:
         return cm & (1 << 0)
+
+
+nothing = NothingIsSolution()
+everything = EverythingIsSolution()
+only_zeros = OnlyZeros()
+
+
+n, m = 2, 2
+num_bits = 2
+unknown = sum(1 << d for d in range(num_bits)) * np.ones((n, m), dtype=Int)
+bm_zero = (1 << 0) * np.ones((n, m), dtype=Int)
+bm_ones = (1 << 1) * np.ones((n, m), dtype=Int)
+
+
+class TestNothingIsSolution:
+    def test_unknown(self) -> None:
+        sol = nothing.solution([unknown])
+        sols = nothing.solutions([unknown])
+        assert sol is None
+        assert sols == []
+
+    def test_bm_zero(self) -> None:
+        sol = nothing.solution([bm_zero])
+        sols = nothing.solutions([bm_zero])
+        assert sol is None
+        assert sols == []
+
+    def test_bm_ones(self) -> None:
+        sol = nothing.solution([bm_ones])
+        sols = nothing.solutions([bm_ones])
+        assert sol is None
+        assert sols == []
+
+
+class TestEverythingIsSolution:
+    def test_unknown(self) -> None:
+        sols = everything.solutions([unknown])
+        assert len(sols) == num_bits ** (n * m)
+
+    def test_bm_zero(self) -> None:
+        sol = everything.solution([bm_zero])
+        sols = everything.solutions([bm_zero])
+        assert np.array_equal(1 << sol, bm_zero)
+        assert len(sols) == 1
+
+    def test_bm_ones(self) -> None:
+        sol = everything.solution([bm_ones])
+        sols = everything.solutions([bm_ones])
+        assert np.array_equal(1 << sol, bm_ones)
+        assert len(sols) == 1
+
+
+class TestOnlyZeros:
+    def test_unknown(self) -> None:
+        sol = only_zeros.solution([unknown])
+        sols = only_zeros.solutions([unknown])
+        assert np.array_equal(1 << sol, bm_zero)
+        assert len(sols) == 1
+
+    def test_bm_zero(self) -> None:
+        sol = only_zeros.solution([bm_zero])
+        sols = only_zeros.solutions([bm_zero])
+        assert np.array_equal(1 << sol, bm_zero)
+        assert len(sols) == 1
+
+    def test_bm_ones(self) -> None:
+        sol = only_zeros.solution([bm_ones])
+        sols = only_zeros.solutions([bm_ones])
+        assert sol is None
+        assert sols == []

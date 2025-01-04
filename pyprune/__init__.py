56f9c7651dd6dbef0c46002f91419f8762a42b59
Miguel Barbosa Pereira <56127923+miguelbper@users.noreply.github.com>
2025-01-04 17:08:40 +0000

    11 small improvements (#14)
    * Change line-lenght and numpy version
    
    * Fix get_rules
    
    * Delete unnecessary files
    
    * Delete unnecessary files (for real now)
    
    * Specify data types
    
    * Add an __all__ to __init__
    
    * Make stack be an input to solution functions

--- pyprune/__init__.py
+++ pyprune/__init__.py
@@ -1,11 +1,3 @@
-from .backtracking import Backtracking, Choices, Grid, rule  # noqa: F401
-from .subset import (  # noqa: F401
-    elements,
-    is_empty,
-    is_singleton,
-    num_elements,
-    remove,
-    remove_except,
-    smallest,
-    subset,
-)
+from .backtracking import ArrayBitMask, ArrayInt, Backtracking, BitMask, Int, rule
+
+__all__ = [Int, BitMask, ArrayBitMask, ArrayInt, rule, Backtracking]
--- test/test_integration.py
+++ test/test_integration.py
@@ -1,11 +1,4 @@
-import os
-
-import numpy as np
-import pytest
-
 from pyprune.backtracking import Backtracking, Choices
-from pyprune.examples.sudoku import Sudoku, is_sudoku_solution, parse_file_to_sudoku
-from pyprune.subset import num_elements
 
 
 class NothingIsSolution(Backtracking):
@@ -33,51 +26,3 @@ class OnlyZeros(Backtracking):
     @staticmethod
     def prune(cm: Choices) -> Choices | None:
         return cm & (1 << 0)
-
-
-@pytest.fixture(params=[1, 2, 5], ids=lambda x: f"[n={x}]")
-def n(request) -> int:
-    return request.param
-
-
-@pytest.fixture(params=[2, 8], ids=lambda x: f"[k={x}]")
-def k(request) -> int:
-    return request.param
-
-
-@pytest.fixture(params=list(range(5)), ids=lambda x: f"[seed={x}]")
-def cm(request, n: int, k: int) -> Choices:
-    rng = np.random.default_rng(request.param)
-    return rng.integers(1, 2**k, (n, n), dtype=np.int32)
-
-
-class TestIntegration:
-    def test_nothing_is_solution(self, cm: Choices):
-        problem = NothingIsSolution(cm)
-        assert problem.solution() is None
-        assert problem.solutions() == []
-
-    def test_everything_is_solution(self):
-        rng = np.random.default_rng(1337)
-        cm = rng.integers(1, 2**2, (2, 2), dtype=np.int32)
-        problem = EverythingIsSolution(cm)
-        num_choices = np.vectorize(num_elements)(cm)
-        assert len(problem.solutions()) == np.prod(num_choices)
-
-    def test_only_zeros(self, cm: Choices):
-        zeros = np.zeros(cm.shape, dtype=np.int32)
-        ones = np.ones(cm.shape, dtype=np.int32)
-        cm += np.where(cm % 2, zeros, ones)
-        problem = OnlyZeros(cm)
-        soln = problem.solution()
-        assert soln is not None
-        assert np.array_equal(soln, zeros)
-        assert len(problem.solutions()) == 1
-
-    def test_sudoku(self):
-        sudokus = parse_file_to_sudoku(os.path.join("pyprune", "examples", "sudoku.txt"))
-        for sudoku in sudokus[:100]:  # file has 10000 sudokus, take only 100
-            problem = Sudoku(sudoku)
-            solution = problem.solution()
-            assert is_sudoku_solution(solution)
-            assert len(problem.solutions()) == 1
